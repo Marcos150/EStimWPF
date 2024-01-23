d327dd64b8eb0d77ca0258fa4ef499ca91b2178d
Marcos Ruiz <marcosruizrubio@gmail.com>
2024-01-23 19:45:38 +0100

    Primer commit

---
+++ EStimWPF/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace EStimWPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
