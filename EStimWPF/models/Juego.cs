69ebca1e7b52bb58df1d636e7622f41fbb0eadef
Marcos Ruiz <marcosruizrubio@gmail.com>
2024-02-06 20:36:42 +0100

    Eliminados usings innecesarios de los modelos

--- EStimWPF/models/Juego.cs
+++ EStimWPF/models/Juego.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EStimWPF.models
+﻿namespace EStimWPF.models
 {
     class Juego
     {
