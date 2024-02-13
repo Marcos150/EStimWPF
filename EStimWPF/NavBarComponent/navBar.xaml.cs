e496a90628fd24189c7fcfb59ab3e06226a38a94
Aitor <Aitor@DESKTOP-DQGQEAH>
2024-02-13 20:55:57 +0100

    Descatados cambiado por 'UserControl' y Navbar creado

--- EStimWPF/NavBarComponent/navBar.xaml.cs
+++ EStimWPF/NavBarComponent/navBar.xaml.cs
@@ -1,8 +1,8 @@
 ﻿using EStimWPF.models;
+using EStimWPF.NavBarComponent.models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,19 +12,20 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace EStimWPF
+namespace EStimWPF.NavBarComponent
 {
     /// <summary>
-    /// Lógica de interacción para Destacados.xaml
+    /// Lógica de interacción para navBar.xaml
     /// </summary>
-    public partial class Destacados : Window
+    public partial class navBar : UserControl
     {
-        public Destacados()
+        public navBar()
         {
+            this.DataContext = new navBarViewModel("link1", "link2", "link3", "link4", "link5", "link6");
             InitializeComponent();
-            this.DataContext = new DestacadosViewModel("GTA 16", "Juegazo");
         }
     }
 }
