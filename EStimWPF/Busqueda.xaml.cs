146d008be53cf4d7d7ac6f02ec2cfc02fcbd1cfe
alexeyki <alexeykr98@gmail.com>
2024-02-13 09:01:42 +0100

    prototipo de lista de juegos

---
+++ EStimWPF/Busqueda.xaml.cs
@@ -0,0 +1,42 @@
+﻿using EStimWPF.models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EStimWPF
+{
+    /// <summary>
+    /// Lógica de interacción para Busqueda.xaml
+    /// </summary>
+    public partial class Busqueda : Page
+    {
+        ObservableCollection<Juego> juegos = new ObservableCollection<Juego>();
+        public Busqueda()
+        {
+            InitializeComponent();
+            for (int i = 0; i < 2; i++)
+            {
+                juegos.Add(new Juego("Juego" + i, "Desarrollador"));
+            }
+            listaJuegos.ItemsSource = juegos;
+        }
+
+        private void CambiarNombre(object sender, RoutedEventArgs e)
+        {
+            juegos[0].Nombre = "jueguito";
+            juegos.Add(new Juego("Juegaaaso", "dessa"));
+        }
+    }
+}
