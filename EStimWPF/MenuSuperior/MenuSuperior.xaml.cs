01787ffcd0b271f5f5b39643a9300031ecc4d1eb
Aitor Moreno <amorenoiborra@gmail.com>
2024-03-11 12:27:49 +0100

    Diseño del card arreglado. Actualizada BBDD con imágenes de los juegos. Agregado logo.

--- EStimWPF/MenuSuperior/MenuSuperior.xaml.cs
+++ EStimWPF/MenuSuperior/MenuSuperior.xaml.cs
@@ -1,14 +1,49 @@
-﻿using System.Windows;
+﻿using System.IO;
+using System.Windows;
 using EStimWPF.models;
 using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 
 namespace EStimWPF.MenuSuperior
 {
     public partial class MenuSuperior : UserControl
     {
+        private NavigationService navigationService;
+        
         public MenuSuperior()
         {
-            this.DataContext = new MenuSuperiorViewModel("logoEnBase64", "CATÁLOGO", "BIBLIOTECA", "PERFIL", "SOPORTE");
+            this.DataContext = new MenuSuperiorViewModel("", "CATÁLOGO", "BIBLIOTECA", "PERFIL", "SOPORTE");
+            InitializeComponent();
+
+            InsertLogoB64();
+        }
+        
+        private void InsertLogoB64()
+        {
+            String logoB64 = ReadBase64String("logoenB64.txt");
+
+            BitmapImage bi = new BitmapImage();
+            bi.BeginInit();
+            byte[] bytes = Convert.FromBase64String(logoB64);
+            
+            bi.StreamSource = new MemoryStream(bytes);
+            bi.EndInit();
+
+            LogoImage.Source = bi;
+        }
+
+        private string ReadBase64String(string filePath)
+        {
+            try
+            {
+                string base64String = File.ReadAllText(filePath);
+                return base64String.Trim();
+            } catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+                return null;
+            }
         }
 
         private void Perfil_Click(object sender, RoutedEventArgs e)
@@ -19,14 +54,14 @@ namespace EStimWPF.MenuSuperior
         
         private void Catalogo_Click(object sender, RoutedEventArgs e)
         {
-            //Busqueda busqueda = new Busqueda();
-            //busqueda.Show();
+            navigationService = NavigationService.GetNavigationService(this);
+            navigationService.Navigate(new Uri("CatalogoComponent/Busqueda.xaml", UriKind.Relative));
         }
         
         private void Biblioteca_Click(object sender, RoutedEventArgs e)
         {
-            //Biblioteca biblioteca = new Biblioteca();
-            //biblioteca.Show();
+            navigationService = NavigationService.GetNavigationService(this);
+            navigationService.Navigate(new Uri("BibliotecaComponent/Biblioteca.xaml", UriKind.Relative));
         }
     }
 }
